896f23a5b5cc39d6997b533279f8d0f282768ab1
Arthur Skorokhid <arthur.skorokhid@gmail.com>
2025-05-21 12:29:27 +0300

    Add Category and Recipe models

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
+    def __iter__(self):
+        return iter(self.name)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
