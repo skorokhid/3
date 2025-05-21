8463c49e8fa02f31a55fd0386ea45a3e6cc495b0
Arthur Skorokhid <arthur.skorokhid@gmail.com>
2025-05-21 12:30:15 +0300

    Add unit tests for Category and Recipe models

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Category, Recipe
 
-# Create your tests here.
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Desserts")
+
+    def test_category_creation(self):
+        self.assertEqual(self.category.name, "Desserts")
+        self.assertIsInstance(iter(self.category), str)
+
+class RecipeTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Main Dishes")
+        self.recipe = Recipe.objects.create(
+            title="Pasta",
+            description="Delicious pasta dish",
+            instructions="Cook pasta...",
+            ingredients="Pasta, sauce",
+            category=self.category
+        )
+
+    def test_recipe_creation(self):
+        self.assertEqual(self.recipe.title, "Pasta")
+        self.assertEqual(self.recipe.category, self.category)
